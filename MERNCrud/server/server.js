de6368cff843d399f9170b4fa0961c5b2d42edfe
pavincius <30844451+PAVincius@users.noreply.github.com>
2023-02-16 12:04:38 -0300

    :sparkles: feat: Server rodando

--- MERNCrud/server/server.js
+++ MERNCrud/server/server.js
@@ -11,10 +11,82 @@ app.listen(PORT, () => {
 
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false);
+
 const DB = 'mongodb+srv://clusterpavincius:y2s43cOFmeI12KBB@cluster0.nij4pw1.mongodb.net/test'
 mongoose.connect(DB, {
     useNewUrlParser: true,
      useUnifiedTopology: true,
 }).then(() =>{
     console.log('Database connected..')
+})
+
+const Patient = require('./model/Patient')
+
+app.post('/add-patient', async(req,res) => {
+    const patientData = new Patient(req.body)
+    try{
+        await patientData.save()
+        res.status(201).json({
+            status: 'Success',
+            data : {
+                patientData
+            }
+        })
+    }catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message : err
+        })
+    } 
+})
+
+app.get('/get-patient', async (req,res) => {
+    const patientDatas = await Patient.find({})
+    try{
+        res.status(200).json({
+            status : 'Success',
+            data : {
+                patientDatas
+            }
+        })
+    }catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message : err
+        })
+    }
+})
+
+app.patch('/update-patient/:id', async (req,res) => {
+    const updatedPatient = await Patient.findByIdAndUpdate(req.params.id,req.body,{
+        new : true,
+        runValidators : true
+      })
+    try{
+        res.status(200).json({
+            status : 'Success',
+            data : {
+                updatedPatient
+            }
+          })
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.delete('/delete-patient/:id', async(req,res) => {
+    await Patient.findByIdAndDelete(req.params.id)
+    
+    try{
+      res.status(204).json({
+          status : 'Success',
+          data : {}
+      })
+    }catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message : err
+        })
+    }
 })
